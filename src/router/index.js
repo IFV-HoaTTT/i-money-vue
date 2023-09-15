0ed97aaccc39b004627edf825e09bebde8c3fb0c
Hoa-TTT <thanhhoa0730@gmail.com>
2023-09-15 10:12:38 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  { path: '/',
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ '../views/index.vue'),
+  },
+  { path: '/register',
+    name: "Register",
+    component: () => import(/* webpackChunkName: "home" */ '../views/register.vue'),
+  },
+]
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+})
+
+export default router;
---
+++ src/views/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>
+        About Layout
+    </h1>
+    <slot />
+</template>
+
+<script>
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "About",
+};
+</script>
